ea3fd39c2f30fc58f04399d55a4a6970d2834f6e
David R. <davidrubio97@outlook.com>
2024-08-22 19:47:17 +0200

    update

--- example code/send_message.vhd
+++ example code/send_message.vhd
@@ -17,13 +17,38 @@ architecture Behavioral of send_message is
 type fsm is (IDLE, SEND_CORRECTION, SEND_DUMMY, SEND_HEADER, SEND_SEQ_NUM, SEND_CTRL, SEND_DATA, SEND_CHECK, SEND_DATA_END);
 signal state : fsm;
 signal cont : unsigned(7 downto 0);
+signal data_s : std_logic_vector(31 downto 0);
+COMPONENT ila_1
+
+PORT (
+	clk : IN STD_LOGIC;
+
+
+
+	probe0 : IN STD_LOGIC;
+	probe1 : IN STD_LOGIC_VECTOR(31 DOWNTO 0)
+);
+END COMPONENT  ;
 
 begin
+
+your_instance_name : ila_1
+PORT MAP (
+	clk => clk,
+
+
+
+	probe0 => rst_n,
+	probe1 => data_s
+);
+
+    data <= data_s;
     
     process(rst_n, clk)
     begin
         if rst_n = '0' then
             state <= IDLE;
+            data_s <= (others=>'0');
         elsif rising_edge(clk) then
             case state is
                 when IDLE =>
@@ -31,29 +56,31 @@ begin
                         state <= SEND_CORRECTION;
 --                    end if;
                 when SEND_CORRECTION =>
-                    data <= x"F7F7F7F7";
+                    data_s <= x"F7F7F7F7";
                     state <= SEND_DUMMY;
                 when SEND_DUMMY =>
-                    data <= x"FF000055";
+                    data_s <= x"FF000055";
                     state <= SEND_HEADER;
                 when SEND_HEADER =>
-                    data <= x"FF0000BC";
+                    data_s <= x"FF0000BC";
                     state <= SEND_SEQ_NUM;
                 when SEND_SEQ_NUM =>
-                    data <= (others=>'0');
+                    data_s <= (others=>'0');
                     state <= SEND_CTRL;
                 when SEND_CTRL =>
-                    data <= (0=>'1', 31=> '1',others=>'0');
+                    data_s <= (0=>'1', 31=> '1',others=>'0');
                     state <= SEND_DATA;
                 when SEND_DATA =>
-                    data <= std_logic_vector(cont) & std_logic_vector(cont) & std_logic_vector(cont) & std_logic_vector(cont);
+                    data_s <= std_logic_vector(cont) & std_logic_vector(cont) & std_logic_vector(cont) & std_logic_vector(cont);
                     if cont = (cont'range=>'1') then
                         state <= SEND_CHECK;
                     end if;
                 when SEND_CHECK =>
-                    data <= (0=>'1', others=>'0');                    
+                    data_s <= (0=>'1', others=>'0');  
+                    state <= SEND_DATA_END;                  
                 when SEND_DATA_END =>
-                    data <= (others=>'0');
+                    data_s <= (others=>'0');
+                    state <= IDLE;
             end case;
         end if;
     end process;
